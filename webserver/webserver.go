42e99869e868c330cbe0476646b5cb62fcb3db19
masahiko tokita <tokita-masahiko@cyber-z.co.jp>
2017-11-02 12:03:04 +0900

    web server sample

---
+++ webserver/webserver.go
@@ -0,0 +1,21 @@
+
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "This is go web server.")
+  })
+
+  http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World")
+  })
+
+  http.ListenAndServe(":8080", nil)
+}
+
+
